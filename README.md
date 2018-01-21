40ecef163b92b75a79a6e2d2458bbe3c6f83de0c
tzaeru <tzaeru@gmail.com>
2018-01-21 21:32:44 +0200

    Work on threaded version. This turned out to be a fun practice!

--- README.md
+++ README.md
@@ -1,20 +1,36 @@
 # VSD-RS
 Very Simple Database in Rust. Really, *very* simple.
 
+VSD is a key-value in-memory database, which also periodically dumps its contents to disk.
+
 WIP, still going to add examples.
 
-But basically you can do this:
-
+But basically you can do this to write a value:
 ```
         let mut vsd = VSD::<u16>::new();
         vsd.open("test_db.db");
         vsd.write("test", 8u16);
 ```
 
+And this to read a value:
 ```
         let mut vsd = VSD::<u16>::new();
         vsd.open("test_db.db");
         assert_eq!(*(vsd.read("test").unwrap()), 8);
+```
+
+If you don't want to write anything to disk, you can omit opening a database:
+```
+        let mut vsd = VSD::<u16>::new();
+       	vsd.write("test", 8u16);
+        assert_eq!(*(vsd.read("test").unwrap()), 8);
+```
+
+And if you prefer a caching system where writing to disk happens in its own thread, you can do:
+```
+        let mut vsd = VSD::<u16>::new();
+        vsd.open_with_caching("test_db.db");
+        vsd.write("test", 8u16);
 ```
 
 It works over basically all standard primitive types and also various types in std.
--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,58 @@ use std::option::Option;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::io::Read;
-use std::fs::metadata;
+use std::fs::File;
+use std::io;
+use std::thread;
+use std::time::{Duration, Instant};
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::AtomicBool;
+use std::marker::PhantomData;
 
 use bincode::{serialize, deserialize, Infinite};
 
-pub struct VSD<T: serde::Serialize + serde::de::DeserializeOwned> {
+pub trait VSDLocked {}
+pub trait VSDUnlocked {}
+
+// Inner thing. Can we avoid using this?
+/*
+pub struct VSD<'a, T: 'a + serde::Serialize + serde::de::DeserializeOwned, LOCK> {
+	inner: &'a mut Arc<VSDInner<T, LOCK>>
+}*/
+
+struct VSD<T: 'static + serde::Serialize + serde::de::DeserializeOwned, LOCK> {
+	/// Sync data structure used when VSD operates in a non-locking mode
 	data: HashMap<String, T>,
-	file: String
+	/// Sync handle to database file used when VSD operates in a non-locking mode
+	file: io::Result<File>,
+	/// Mutex'd data structure used when VSD operates in a locking mode
+	data_locked: Arc<Mutex<HashMap<String, T>>>,
+	/// Mutex'd handle to database file used when VSD operates in a locking mode
+	file_locked: Arc<Mutex<io::Result<File>>>,
+	/// JoinHandle for a caching thread used to flush periodically to disk
+	caching_thread: Option<thread::JoinHandle<()>>,
+	/// Used to indicate whether we've flushed to disk after last write
+	dirty: AtomicBool,
+	/// When we last wrote to the database
+	last_written: Arc<Mutex<Instant>>,
+	/// When we last flushed the database
+	last_flushed: Arc<Mutex<Instant>>,
+	/// Used to get rid of "parameter 'LOCK' is never used" error from compiler.
+	phantom_lock: PhantomData<LOCK>,
 }
 
-impl <T: serde::Serialize + serde::de::DeserializeOwned> VSD<T> {
-    pub fn new() -> VSD<T> {
+impl <T: serde::Serialize + serde::de::DeserializeOwned, LOCK: VSDLocked> VSD<T, LOCK> {
+    pub fn new() -> VSD<T, LOCK> {
         VSD {
         	data: HashMap::new(),
-        	file: "".to_string()
+        	file: Err(io::Error::new(io::ErrorKind::NotFound, "Database file not opened")),
+        	data_locked: Arc::new(Mutex::new(HashMap::new())),
+        	file_locked: Arc::new(Mutex::new(Err(io::Error::new(io::ErrorKind::NotFound, "Database file not opened")))),
+        	caching_thread: None,
+        	dirty: AtomicBool::new(false),
+        	last_written: Arc::new(Mutex::new(Instant::now())),
+			last_flushed: Arc::new(Mutex::new(Instant::now())),
+			phantom_lock: PhantomData
         }
     }
 
@@ -28,24 +66,20 @@ impl <T: serde::Serialize + serde::de::DeserializeOwned> VSD<T> {
 	/// If file doesn't exist, a new db is created.
 	pub fn open(&mut self, file_name: &str)
 	{
-		let mut file = OpenOptions::new().read(true).write(true).create(true).open(file_name).unwrap();
-		//let md = metadata(file_name).unwrap();
-
-		// Unfortunately read_to_end fails if there's nothing to read, i.e. if the file is empty.
-		// Soo we do this ugly hack to check if the file is larger than 0 bytes in size.
+		self.file = OpenOptions::new().read(true).write(true).create(true).open(file_name);
 
 		let mut decode_buf = Vec::new();
-		let to_read = file.read_to_end(&mut decode_buf).unwrap();
-		self.data = deserialize(&decode_buf[..]).unwrap();
+		match self.file
+		{
+			// Must use references, or else ownership of File moves to match
+			Ok(ref mut file) => {
+				let _ = (*file).read_to_end(&mut decode_buf).unwrap();
+				self.data = deserialize(&decode_buf[..]).unwrap();
+			}
+			Err(_) => {
 
-		self.file = file_name.to_string();
-	}
-	/// Opens and reads a database from file.
-	/// If file doesn't exist, a new db is created.
-	/// This will spawn a thread to periodically flush several writes to disk at once instead of every write flushing.
-	pub fn open_with_caching(&self, file_name: &str)
-	{
-
+			}
+		}
 	}
 
 	/// Writes to database.
@@ -60,11 +94,71 @@ impl <T: serde::Serialize + serde::de::DeserializeOwned> VSD<T> {
 		return self.data.get(key);
 	}
 
-	fn flush(&self)
+	fn flush(&mut self)
 	{
 		let encoded: Vec<u8> = serialize(&self.data, Infinite).unwrap();
-		let mut file = OpenOptions::new().write(true).create(true).open(&self.file).unwrap();
-		file.write_all(&encoded[..]);
+
+		match self.file
+		{
+			// Must use references, or else ownership of File moves to match
+			Ok(ref mut file) => {
+				file.write_all(&encoded[..]);
+			}
+			Err(_) => {
+
+			}
+		}
+	}
+}
+
+impl <T: serde::Serialize + serde::de::DeserializeOwned, VSDLocked>Drop for VSD<T, VSDLocked> {
+	fn drop(&mut self) {
+        println!("Dropping.");
+    }
+}
+
+impl <T: serde::Serialize + serde::de::DeserializeOwned + Send + Sync, LOCK: VSDLocked + Send> VSD<T, LOCK> {
+	/// Opens and reads a database from file.
+	/// If file doesn't exist, a new db is created.
+	/// This will spawn a thread to periodically flush several writes to disk at once instead of flushing on every write
+	pub fn open_with_caching(&mut self, file_name: &str)
+	{
+		self.file_locked = Arc::new(Mutex::new(OpenOptions::new().read(true).write(true).create(true).open(file_name)));
+
+		let mut decode_buf = Vec::new();
+		match self.file
+		{
+			// Must use references, or else ownership of File moves to match
+			Ok(ref mut file) => {
+				let _ = (*file).read_to_end(&mut decode_buf).unwrap();
+				self.data = deserialize(&decode_buf[..]).unwrap();
+			}
+			Err(_) => {
+
+			}
+		}
+
+		//let local_self = Arc::new(self).clone();
+		let data_clone = self.data_locked.clone();
+		let file_clone = self.file_locked.clone();
+		self.caching_thread = Some(thread::spawn(move || {
+			let data = data_clone.lock().unwrap();
+			let encoded: Vec<u8> = serialize(&(*data), Infinite).unwrap();
+			println!("{:?}", encoded[0]);
+
+			// Must do the name bind or else we'll have a bug here.
+			let mut file_lock = file_clone.lock().unwrap();
+
+			match *file_lock
+			{
+				Ok(ref mut file) => {
+					file.write_all(&encoded[..]);
+				}
+				Err(_) => {
+
+				}
+			}
+		}));
 	}
 }
 
@@ -81,7 +175,7 @@ mod tests {
 
     #[test]
     fn test_writing_reading_integers() {
-        let mut vsd = VSD::<u16>::new();
+        let mut vsd = VSD::<u16, VSDUnlocked>::new();
         vsd.open("test1");
         vsd.write("write_test", 8u16);
         // Test for a true value
@@ -92,7 +186,7 @@ mod tests {
 
     #[test]
     fn test_writing_reading_strings() {
-        let mut vsd = VSD::<String>::new();
+        let mut vsd = VSD::<String, VSDUnlocked>::new();
         vsd.open("test2");
         vsd.write("write_test", "hello".to_string());
         // Test for a true value
@@ -100,7 +194,7 @@ mod tests {
 		// Test for a false value
 		assert_ne!(*(vsd.read("write_test").unwrap()), "goodbye".to_string());
 		
-		vsd = VSD::<String>::new();
+		vsd = VSD::<String, VSDUnlocked>::new();
 		vsd.open("test2");
 		// Test for a true value
 		assert_eq!(*(vsd.read("write_test").unwrap()), "hello".to_string());
